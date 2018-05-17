098c44fe027dece82337fd1b39fb0bf9271b0968
al132mc <apbertram1@gmail.com>
2018-05-17 14:30:48 -0500

    v1.0.1

--- src/main/kotlin/al132/alib/utils/extensions/List.kt
+++ src/main/kotlin/al132/alib/utils/extensions/List.kt
@@ -2,6 +2,7 @@ package al132.alib.utils.extensions
 
 import com.google.common.collect.ImmutableList
 import net.minecraft.item.ItemStack
+import net.minecraftforge.oredict.OreDictionary
 import java.util.*
 
 /**
@@ -13,7 +14,11 @@ fun <E> List<E>.toImmutable(): ImmutableList<E> = ImmutableList.Builder<E>().add
 
 fun List<ItemStack>.containsStack(stack: ItemStack): Boolean = any { ItemStack.areItemStacksEqual(it, stack) }
 
-fun List<ItemStack>.containsItem(stack: ItemStack): Boolean = any { ItemStack.areItemsEqual(it, stack) }
+//fun List<ItemStack>.containsItem(stack: ItemStack): Boolean = any { ItemStack.areItemsEqual(it, stack) }
+
+fun List<ItemStack>.containsItem(stack: ItemStack, strict: Boolean = false): Boolean =
+        any { OreDictionary.itemMatches(it, stack, strict) }
+
 
 fun List<ItemStack>.copy(): List<ItemStack> {
     val temp = ArrayList<ItemStack>()
